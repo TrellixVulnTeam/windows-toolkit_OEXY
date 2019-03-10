48e4ac4acbcc837c6d605bd08bfc2e01f9776b6f
Sławek <skaczmarek88@gmail.com>
2019-03-10 01:52:57 +0100

    updated build script

--- build.bat
+++ build.bat
@@ -4,5 +4,6 @@ pyinstaller --noconfirm --onedir ^
 	--workpath ".\output\build" ^
 	--distpath  ".\output\dist" ^
 	--add-data ".\virtualenvironment\Lib\site-packages\pyfiglet";.\pyfiglet  ^
+	--add-data ".\app";.\app  ^
 	windows-toolkit.py
 pause
