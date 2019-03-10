41fbd7bd76d5641ad2101a7c93974c188b21f66f
Sławek <skaczmarek88@gmail.com>
2019-03-10 02:06:31 +0100

    fixed build script error

--- build.bat
+++ build.bat
@@ -2,8 +2,9 @@
 call .\virtualenvironment\Scripts\activate.bat
 pyinstaller --noconfirm --onedir ^
 	--workpath ".\output\build" ^
-	--distpath  ".\output\dist" ^
-	--add-data ".\virtualenvironment\Lib\site-packages\pyfiglet";.\pyfiglet  ^
-	--add-data ".\app";.\app  ^
+	--distpath ".\output\dist" ^
+	--specpath ".\output" ^
+	--add-data "..\virtualenvironment\Lib\site-packages\pyfiglet";."\pyfiglet" ^
+	--add-data "..\app";."\app" ^
 	windows-toolkit.py
 pause
